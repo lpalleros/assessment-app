5ca7df3de58d83ffb85b220d6d73010f32e31343
RodneyMac <rodneydavidmaciel@gmail.com>
2021-06-05 16:35:15 -0300

    components-css

---
+++ src/components/StartForm/StartForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StartForm.css';
+
+const StartForm = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default StartForm;
--- src/screen/Assessment/Assessment.js
+++ src/screen/Assessment/Assessment.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import './Assessment.css';
 
 function Assessment () {
     return (
