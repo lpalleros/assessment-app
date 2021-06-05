07ab499feba09b7394fd58472cb3e64461b0f849
lpalleros <lucaspalleros@gmail.com>
2021-06-05 00:19:46 -0300

    directorios

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import ProgressBar from '../ProgressBar/ProgressBar'
 
 function App() {
   return (
     <div className="App">
       <h2>App</h2>
+      <ProgressBar />
     </div>
   );
 }
---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,9 @@
+import './ProgressBar.css'
+
+export default function ProgressBar () {
+    return (
+        <div>
+            <h2>ProgressBar </h2>
+        </div>
+    )
+}
