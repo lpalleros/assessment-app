056368eb049c9238f98de3d9cf43c156775775e5
RodneyMac <rodneydavidmaciel@gmail.com>
2021-06-05 16:14:05 -0300

    Vistas

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Assessment from '../screen/Assessment/Assessment'
+import Start from '../screen/Start/Start';
+import Dashboard from '../screen/Dashboard/Dashboard';
 
 
 function App() {
   return (
     <div className="App">
       <Assessment/>
+      <Start/>
+      <Dashboard/>
     </div>
   );
 }
