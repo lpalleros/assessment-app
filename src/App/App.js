277cae2d8b8febd30c3d9ee7b2934ceb4e9c7feb
RodneyMac <rodneydavidmaciel@gmail.com>
2021-06-05 19:48:50 -0300

    Router

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 import './App.css';
-// import Assessment from '../screen/Assessment/Assessment'
+import Assessment from '../screen/Assessment/Assessment';
 import Start from '../screen/Start/Start';
-// import Dashboard from '../screen/Dashboard/Dashboard';
+import Dashboard from '../screen/Dashboard/Dashboard';
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <Assessment/> */}
-      <Start/>
-      {/* <Dashboard/> */}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Start/>
+        </Route>
+        <Route exact path="/assessment">
+          <Assessment/>
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
