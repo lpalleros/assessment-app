9a7e0b7b7221c5eddbf1461c110ce05da18c19d3
lucaspalleros <lucaspalleros@gmail.com>
2021-06-05 20:24:55 -0300

    assessment form

--- src/screen/Assessment/Assessment.js
+++ src/screen/Assessment/Assessment.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './Assessment.css';
 
 import AssessmentForm from '../../components/AssessmentForm/AssessmentForm';
+import Navbar from '../../components/Navbar/Navbar';
+
 function Assessment () {
     return (
         <>
+            <Navbar/> 
             <AssessmentForm/>
         </>
     )
