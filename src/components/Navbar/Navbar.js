00ec0d5347f866aafb0084cd4198cca65341fe98
RodneyMac <rodneydavidmaciel@gmail.com>
2021-06-11 21:17:34 -0300

    actualización start-component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,18 +5,7 @@ import './Navbar.css';
 function Navbar() {
     return (
         <nav className="navbar">
-            <a href="#" className="toggle-button">
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
-            </a>
-            <div className="navbar-links">
-                <ul>
-                    <Link to="/" className="link">Start</Link>
-                    <Link to="/assessment" className="link">Assessment</Link>
-                    <Link to="/dashboard" className="link">Dashboard</Link>
-                </ul>
-            </div>
+            
         </nav>
     )
 }
--- src/components/StartForm/StartForm.js
+++ src/components/StartForm/StartForm.js
@@ -4,23 +4,26 @@ import {Link} from 'react-router-dom';
 
 const StartForm = () => {
     return (
-        <div className="contenedor">
-            <h1 className="texto-cabecera">Lorem, ipsum dolor sit amet</h1>
-            <div className="lado-izquierdo">
-                <h1>15</h1>
-                <h2>Lorem, ipsum</h2>
-                <h3>Lorem</h3>
+        <>
+            <div className="contenedor">
+                <div className="lado-izquierdo">
+                    <h1>15</h1>
+                    <div>
+                        <h2>Lorem</h2>
+                        <h3>Lorem, ipsum</h3>
+                    </div>
+                </div>
+                <div className="lado-derecho">
+                    <h3>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum iste sunt 
+                    dolores itaque dolore. Iste beatae nulla nam debitis rem sunt tempore, fugit
+                    </h3>
+                    <Link to="/assessment">
+                        <button className="btn-start">Start</button>
+                    </Link>
+                </div>
             </div>
-            <div className="lado-derecho">
-                <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum iste sunt 
-                dolores itaque dolore. Iste beatae nulla nam debitis rem sunt tempore, fugit
-                </p>
-                <Link to="/assessment">
-                    <button className="btn-start">Start</button>
-                </Link>
-            </div>
-        </div>
+        </>
     )
 }
 
